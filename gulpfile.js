098cc10b4e0e003c1efd0c39f2c57271f46aee9f
Katya Belova <katrin.belova@gmail.com>
2018-03-06 19:54:21 +0200

    Added file .travis.yml, modified gulpfile with gulp-sass.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,37 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
 
 
 gulp.task('views', function () {
 	return gulp.src('./src/index.pug')
 		.pipe(pug())
 		.on('error', function(error) {
-     		gutil.log(gutil.colors.red('Error: ' + error.message));
-     		this.emit('end');
-  		});
+			gutil.log(gutil.colors.red('Error: ' + error.message));
+			this.emit('end');
+		});
 		.pipe(gulp.dest('./public'));
 });
 
+gulp.task('styles', function () {
+	return gulp.src(['src/sass/**/*.sass','src/sass/**/*.scss'])
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+		.pipe(gulp.dest('public/css'))
+		.pipe(browserSync.reload({stream: true}))
+});
+// outputStyle: nested, compact, expanded, compressed
+
 gulp.task('watch', function () {
 	gulp.watch('./src/index.pug', gulp.series('views'));
+	gulp.watch(['src/sass/**/*.sass','src/sass/**/*.scss'], gulp.series('styles'))
 });
 
 gulp.task('serve', function () {
-  browserSync.init({
-    server: './public',
-    port: 8080
-  });
+	browserSync.init({
+		server: './public',
+		port: 8080
+});
 
   browserSync.watch('./public/**/*.*').on('change', browserSync.reload);
 });
@@ -32,7 +42,8 @@ gulp.task('clean', function () {
 
 gulp.task('build', gulp.series(
 	'clean',
-	'views'
+	'views',
+	'styles'
 ));
 
 gulp.task('default', gulp.series(
